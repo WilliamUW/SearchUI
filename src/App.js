83c286bb64fd13245785c215d99c2ae783de2a9f
apple <davidxmkong@gmail.com>
2021-12-31 06:02:05 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,119 +1,41 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
 
 import {
   ErrorBoundary,
-  Facet,
-  SearchProvider,
-  SearchBox,
+  Facet, Paging,
   //Results,
   PagingInfo,
-  ResultsPerPage,
-  Paging,
-  Sorting,
+  ResultsPerPage, SearchBox, SearchProvider, Sorting,
   WithSearch
 } from "@elastic/react-search-ui";
-
 import { Layout } from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
-
+import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
+import { Avatar, Card, Modal } from "antd";
+import "antd/dist/antd.css";
+import React, { useState } from 'react';
+import "./additional.css";
 import {
-  buildAutocompleteQueryConfig, // search auto complete
-  buildFacetConfigFromConfig, // filters
-  buildSearchOptionsFromConfig, //
-  buildSortOptionsFromConfig, // sort
+  buildAutocompleteQueryConfig,
+  buildFacetConfigFromConfig,
+  buildSearchOptionsFromConfig,
+  buildSortOptionsFromConfig,
   getConfig,
   getFacetFields
 } from "./config/config-helper";
+import Item from "./Item"
 
-// import { useState } from "react";
-import "antd/dist/antd.css";
-import "./additional.css";
-import { Modal, Button } from "antd";
 
-import { Card } from "antd";
+const { Meta } = Card;
+
+
+
+
+
+
+
 
 // make modal class
 
-const ModalAppTest = (r) => {
-  var isModalVisible = true;
-  /*
-  const showModal = () => {
-    isModalVisible = true;
-  };
-  */
-  function handleOk() {
-    //isModalVisible = false;
-    ReactDOM.render("", document.getElementById(r.id.raw));
-  }
-  const handleCancel = () => {
-    //isModalVisible = false;
-    ReactDOM.render("", document.getElementById(r.id.raw));
-  };
-  console.log(r);
-  r = r.r;
-  var bodyHtml = "";
-  if (r.body_html) {
-    bodyHtml = r.body_html.raw;
-  }
-  return (
-    <>
-      <Modal
-        id="test"
-        title="Selected Email Modal"
-        // style={{ width: "80em" }}
-        visible={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        width={1000}
-        centered
-      >
-        <p
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            margin: "0"
-          }}
-        >
-          <span>
-            <strong
-              dangerouslySetInnerHTML={{
-                __html: innerFormat(r.from.snippet)
-              }}
-            ></strong>
-          </span>
-          <span
-            style={{ color: "grey" }}
-            dangerouslySetInnerHTML={{
-              __html: innerFormat(r.date.snippet)
-            }}
-          ></span>
-        </p>
-
-        <h3
-          dangerouslySetInnerHTML={{
-            __html: innerFormat(r.subject.snippet)
-          }}
-        ></h3>
-        <p
-          style={{ color: "grey" }}
-          dangerouslySetInnerHTML={{
-            __html: "To: " + innerFormat(r.to.snippet)
-          }}
-        ></p>
-        <iframe
-          srcDoc={bodyHtml}
-          style={{ width: "100%", height: "25em", display: "block" }}
-          title="Email Body iframe"
-          id="iframe"
-        ></iframe>
-        <p>{r.mailgunattachments ? r.mailgunattachments.raw : ""}</p>
-      </Modal>
-    </>
-  );
-};
 
 const { hostIdentifier, searchKey, endpointBase, engineName } = getConfig();
 const connector = new AppSearchAPIConnector({
@@ -136,7 +58,7 @@ console.log(config);
 
 var displayStringLength = 100;
 
-export function innerFormat(string) {
+const innerFormat = (string) => {
   var input = string;
   if (input.length > displayStringLength) {
     input = input.substring(0, displayStringLength) + "...";
@@ -150,15 +72,7 @@ export default function App() {
     <SearchProvider config={config}>
       <WithSearch mapContextToProps={({ results }) => ({ results })}>
         {({ results }) => {
-          console.log("wasSearched");
-          console.log(wasSearched);
-          //results.map((r) => modals.push(ModalAppTest(r.subject.raw)));
-          //console.log(modals[0]);
-          console.log("Config after loading results");
-          console.log(config);
-          console.log(config.searchQuery.search_fields);
-          // console.log(JSON.parse(config.searchQuery.search_fields));
-          // console.log(document.getElementById("downshift-0-input").textContent)
+
           return (
             <div className="App">
               <ErrorBoundary>
@@ -192,66 +106,12 @@ export default function App() {
                     <div className="App">
                       <br />
 
-                      {results.map((r) => (
-                        <div
-                          key={r.id.raw}
-                          // onClick={() => displayFullEmail(r)}
-                        >
-                          <Card style={{ width: "auto" }}>
-                            <p
-                              style={{
-                                display: "flex",
-                                justifyContent: "space-between",
-                                margin: "0"
-                              }}
-                            >
-                              <span>
-                                <strong
-                                  dangerouslySetInnerHTML={{
-                                    __html: innerFormat(
-                                      r.from.snippet.split("&lt")[0]
-                                    )
-                                  }}
-                                ></strong>
-                              </span>
-                              <span
-                                style={{ color: "grey" }}
-                                dangerouslySetInnerHTML={{
-                                  __html: innerFormat(r.date.snippet)
-                                }}
-                              ></span>
-                            </p>
+                      {results.map(r=>
+                      <ErrorBoundary>
+                        <Item r={r}/>
+                        </ErrorBoundary>
+                        )}
 
-                            <h3
-                              dangerouslySetInnerHTML={{
-                                __html: innerFormat(r.subject.snippet)
-                              }}
-                            ></h3>
-                            <p
-                              style={{ color: "grey" }}
-                              dangerouslySetInnerHTML={{
-                                __html: innerFormat(r.stripped_text.snippet)
-                              }}
-                            ></p>
-
-                            <Button
-                              type="primary"
-                              onClick={() => displayFullEmail(r)}
-                            >
-                              Open Email Modal
-                            </Button>
-                            <div id={r.id.raw} />
-                            <script>
-                              var mountNode =
-                              document.getElementById('container');
-                            </script>
-                          </Card>
-                          <br />
-                        </div>
-                      ))}
-                      <script>
-                        var mountNode = document.getElementById('container');
-                      </script>
                     </div>
                   }
                   // Result header (e.g. "showing 1-20 out of 1037")
@@ -272,72 +132,3 @@ export default function App() {
   );
 }
 
-export function displayFullEmail(r) {
-  // console.log(document.getElementById(r.id.raw));
-  ReactDOM.render(<ModalAppTest r={r} />, document.getElementById(r.id.raw));
-}
-
-/*
- <div>
-                      <p>Body Content Start</p>
-                      <Results
-                        titleField={getConfig().titleField}
-                        urlField={getConfig().urlField}
-                        thumbnailField={getConfig().thumbnailField}
-                        shouldTrackClickThrough={true}
-                      />
-                      <p>Body Content End</p>
-                    </div>
-                    */
-
-/*
-                    <Results
-                      view={(results) => {
-                        console.log(results);
-                        return (
-                          <>
-                            {results.map((x) => (
-                              <>
-                                <div key={x.id.raw}>
-                                  <Card style={{ width: 300 }}>
-                                    <h1>Email Title {x.subject.raw}</h1>
-                                    <h2>Email Subtitle</h2>
-                                    <p>Card content 1</p>
-                                    <p>Card content 2</p>
-                                    <p>Card content 3</p>
-                                  </Card>
-                                </div>
-                              </>
-                            ))}
-                          </>
-                        );
-                      }}
-                    />
-                    */
-/*
-                    <Results
-                      view={(wasSearched) => {
-                        console.log(wasSearched);
-                        console.log(wasSearched.children);
-                        return (
-                          <>
-                            {wasSearched.map((x) => (
-                              <>
-                                <div>
-                                  <Card style={{ width: 300 }}>
-                                    <h1>Email Title {x.subject.raw}</h1>
-                                    <h2>Email Subtitle</h2>
-                                    <p>Card content 1</p>
-                                    <p>Card content 2</p>
-                                    <p>Card content 3</p>
-                                    <ModalAppTest />
-                                  </Card>
-                                </div>
-                              </>
-                            ))}
-                          </>
-                        );
-                      }}
-                      renderResult={(wasSearched) => <>test render</>}
-                    />
-                    */
---
+++ src/Item.js
@@ -0,0 +1,114 @@
+
+
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
+import { Avatar, Card, Modal } from "antd";
+import "antd/dist/antd.css";
+import React, { Component } from 'react';
+import "./additional.css";
+
+
+
+
+const { Meta } = Card;
+
+
+
+const innerFormat = (string) => {
+  var input = string;
+  input = input.replace(/ *\([^)]*\) */g, "")
+  input = input.replace(/ *\([^)]*$ */g, "")
+  input = input.replace(/\[.*\]/g, '')
+  input = input.replace(/d@mg.glas.vin/g, "")
+  return <span dangerouslySetInnerHTML={{__html:input}}/>;
+}
+
+
+class Item extends Component {
+  constructor(props) {
+    // Required step: always call the parent class' constructor
+    super(props);
+
+    // Set the state directly. Use props if necessary.
+    this.state = {
+      isModalVisible: false
+    }
+  }
+
+
+  
+  showModal = () => {
+    this.setState({isModalVisible:true})
+  };
+
+  handleOk = () => {
+    this.setSstate({isModalVisible:false})
+  };
+
+  handleCancel = () => {
+    this.setState({isModalVisible:false})
+  };
+  render() {
+
+    let r = this.props.r
+    return (
+
+      <div>
+          <a
+          onClick={this.showModal}
+
+          >
+            <Card style={{ width: "auto" }}
+            title={r.subject&&innerFormat(r.subject.snippet)} extra={r.date&&r.date.snippet}
+            >
+            <Meta
+          avatar={<Avatar src={"https://logo.clearbit.com/"+r.from.snippet.split('@')[1].split('>')[0]} />}
+          title={r.from&&innerFormat(r.from.snippet)}
+          description={r.body_plain&&innerFormat(r.body_plain.snippet)}
+        />
+    
+    
+    
+    
+    
+            </Card>
+            <br />
+          </a>
+    
+          <Modal
+            id="test"
+            title={innerFormat(r.subject.snippet)}
+            // style={{ width: "80em" }}
+            visible={this.state.isModalVisible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+            width={1000}
+            centered
+          >
+            <p
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                margin: "0"
+              }}
+            >
+              {innerFormat(r.from.snippet)}
+              {innerFormat(r.date.snippet)}
+            </p>
+    
+            
+            
+            {r.stripped_html&&<iframe
+              srcDoc={r.stripped_html.raw.replace(/href=".+?"/g,"")}
+              style={{ width: "100%", height: "60vh", display: "block" }}
+              title="Email Body iframe"
+              id="iframe"
+            ></iframe>}
+            <p>{r.mailgunattachments ? r.mailgunattachments.raw : ""}</p>
+          </Modal>
+      </div>
+    );
+  }
+}
+
+export default Item;
+
